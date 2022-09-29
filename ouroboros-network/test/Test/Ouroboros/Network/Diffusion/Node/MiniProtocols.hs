b93680ef58ab8841a3e3cb9a544e286f4c148e75
Armando Santos <armando@well-typed.com>
2022-09-29 17:02:33 +0100

    Propagate Hanshake changes to PeerSelection Gov
    - Add peerConnToPeerSharing in order to be able to retrieve it from a
    polymorphic peerconn data type
    - Create auxiliary functions in KnownPeers module to update PeerSharing
      value of a particular address
    - Update PeerSharing value on coldPeerPromotion
    - Refactoreed tests to use PeerSharing values
      - Introduce PeerSharingScript

--- ouroboros-network/test/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
+++ ouroboros-network/test/Test/Ouroboros/Network/Diffusion/Node/MiniProtocols.hs
@@ -200,9 +200,9 @@ applications :: forall block header m.
              -> Codecs block m
              -> LimitsAndTimeouts block
              -> AppArgs m
-             -> m (Diff.Applications NtNAddr NtNVersion NtNVersionData
-                                     NtCAddr NtCVersion NtCVersionData
-                                     m ())
+             -> Diff.Applications NtNAddr NtNVersion NtNVersionData
+                                  NtCAddr NtCVersion NtCVersionData
+                                  m ()
 applications debugTracer nodeKernel
              Codecs { chainSyncCodec, blockFetchCodec
                     , keepAliveCodec, pingPongCodec }
@@ -213,16 +213,16 @@ applications debugTracer nodeKernel
                , aaKeepAliveStdGen
                , aaKeepAliveInterval
                , aaPingPongInterval
-               }
-             = do
-    return $ Diff.Applications
+               , aaOwnPeerSharing
+               } =
+    Diff.Applications
       { Diff.daApplicationInitiatorMode =
           simpleSingletonVersions UnversionedProtocol
-                                  (NtNVersionData InitiatorOnlyDiffusionMode)
+                                  (NtNVersionData InitiatorOnlyDiffusionMode aaOwnPeerSharing)
                                   initiatorApp
       , Diff.daApplicationInitiatorResponderMode =
           simpleSingletonVersions UnversionedProtocol
-                                  (NtNVersionData aaDiffusionMode)
+                                  (NtNVersionData aaDiffusionMode aaOwnPeerSharing)
                                   initiatorAndResponderApp
       , Diff.daLocalResponderApplication =
           simpleSingletonVersions UnversionedProtocol
